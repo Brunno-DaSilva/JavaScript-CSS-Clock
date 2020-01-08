274d14d5b5b385db9b346fce88f28e62bd4ee8ad
LeandroDaSilva Bruno <USDASILVAB@tetrapak.com>
2020-01-08 06:58:32 -0600

    Added js logic

---
+++ main.js
@@ -0,0 +1,30 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+const setDate = () => {
+  //Get Date method
+  const now = new Date();
+
+  //Get Seconds
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+
+  //Get Minutes
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+
+  //Get Hours
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 60) * 360 + 90;
+
+  //Display logic
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+
+  console.log(`${hours} - ${minutes} - ${seconds}`);
+  // console.log(secondsDegrees);
+};
+
+setInterval(setDate, 1000);
