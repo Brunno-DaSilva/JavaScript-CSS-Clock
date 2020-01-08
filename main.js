ad2b3098003c990c2e89241885c282c6bd8931dd
LeandroDaSilva Bruno <USDASILVAB@tetrapak.com>
2020-01-08 13:04:57 -0600

    Added a second digital clock and style

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const secondHand = document.querySelector(".second-hand");
-const minHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand");
+const second = document.querySelector(".second");
+const min = document.querySelector(".min");
+const hour = document.querySelector(".hour");
+
+const digitalClock = document.querySelector(".time")
 
 const setDate = () => {
   //Get Date method
@@ -8,22 +10,24 @@ const setDate = () => {
 
   //Get Seconds
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const secondsDegrees = (seconds / 60) * 360;
 
   //Get Minutes
   const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+  const minutesDegrees = ((minutes / 60) * 360);
 
   //Get Hours
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
+  const hoursDegrees = ((hours / 12) * 360);
 
   //Display logic
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+  second.style.transform = `rotate(${secondsDegrees}deg)`;
+  min.style.transform = `rotate(${minutesDegrees}deg)`;
+  hour.style.transform = `rotate(${hoursDegrees}deg)`
 
   console.log(`${hours} - ${minutes} - ${seconds}`);
+
+  digitalClock.innerHTML = `<span>${hours}:</span> <span>${minutes}:</span> <span>${seconds}</span>`
   // console.log(secondsDegrees);
 };
 
